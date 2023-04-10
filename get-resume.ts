e48f7d3faa9be0ed7cc2a7e799cf85ca12a53e91
Arthur Bufalo <arthurbr@cdmd.dev>
2023-04-09 21:06:54 -0300

    wtf bro

--- get-resume.ts
+++ get-resume.ts
@@ -235,13 +235,13 @@ main()
 
 // Please don't kill me for this
 let funCounter = 0;
-process.on('uncaughtException', async () => {
+process.on('uncaughtException', async e => {
   if (funCounter >= 10) {
     console.error("Ending the fun as it's probably other people's fault")
     process.exit(69)
-    return;
   }
   await browser.close()
+  console.error(e)
   console.log("Restarting thread for the", ++funCounter, "time")
   main()
 })
