6f4e4e49d492b684eade59dd39bcf7f627f564f5
Ar7hurz1nh0 <arthurbr@cdmd.dev>
2024-01-01 13:59:43 -0300

    made my age dynamic on the landing page too

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,16 @@ import Link from "next/link";
 import React from "react";
 // import { GeistMono } from 'geist/font/mono';
 
+const now = Date.now();
+const birth = (new Date(2004, 11, 28)).getTime();
+
+const calcYears = (startDate: number) => {
+  const diffTime = Math.abs(now - startDate);
+  const diffYears = diffTime / (1000 * 60 * 60 * 24 * 365);
+  return Math.floor(diffYears);
+};
+
+
 const Index = () => {
   return (
     <>
@@ -16,7 +26,7 @@ const Index = () => {
             <h3 className="font-extrabold md:text-8xl text-6xl opacity-20">Web Developer<br />System Engineer</h3>
           </div>
           <p className="text-justify text-xl md:w-full w-2/3 max-md:pl-3 max-sm:w-11/12">
-            I am a 19 years old Brazilian programmer, a full-stack web developer and also a system engineer, where I make native apps. I also like photography, music, cinema, all sorts of paintings, and learn new things, and I am always looking for new challenges.
+            I am a {calcYears(birth)} years old Brazilian programmer, a full-stack web developer and also a system engineer, where I make native apps. I also like photography, music, cinema, all sorts of paintings, and learn new things, and I am always looking for new challenges.
           </p>
           <div className="mt-12 mr-12 block text-right">
             <Link href="/cv" className="py-4 px-8 rounded-lg dark:bg-zinc-200 bg-zinc-800 text-zinc-200 hover:text-zinc-200 dark:text-zinc-800 font-bold group">
