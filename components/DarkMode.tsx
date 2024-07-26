39064632b865bfc26c28bff0dac7834de3696b2d
Ar7hurz1nh0 <arthurbr@cdmd.dev>
2024-07-26 01:51:43 -0300

    remade theme switcher

--- components/DarkMode.tsx
+++ components/DarkMode.tsx
@@ -1,51 +1,49 @@
+import { Switch } from "@nextui-org/react";
 import { useEffect, useState } from "react";
-import { Switch } from "@headlessui/react";
 
 export default function DarkMode() {
-  const [enabled, setEnabled] = useState<boolean>(
-    typeof localStorage === "undefined"
-      ? true
-      : localStorage.getItem("dark-mode") !== null
-        ? (JSON.parse(
-            localStorage.getItem("dark-mode") ??
-              ("null" satisfies string | null),
-          ) as boolean | null) ??
-          document.documentElement.classList.contains("dark")
-        : true,
+  if (typeof localStorage === "undefined") return null;
+  const [disabled, setDisabled] = useState<boolean>(
+    localStorage.getItem("theme") === null
+      ? !window.matchMedia("(prefers-color-scheme: dark)").matches
+      : localStorage.getItem("theme") === "light",
   );
   useEffect(() => {
-    console.debug(enabled ? "Enabling dark mode" : "Disabling dark mode");
-    if (enabled) {
-      document.documentElement.classList.add("dark");
-      document.documentElement.classList.remove("light");
-      localStorage.setItem("dark-mode", "true");
-    }
-    if (!enabled) {
+    console.debug(disabled ? "Disabling dark mode" : "Enabling dark mode");
+    if (disabled) {
       document.documentElement.classList.add("light");
       document.documentElement.classList.remove("dark");
-      localStorage.setItem("dark-mode", "false");
+      localStorage.setItem("theme", "light");
     }
-  }, [enabled]);
+    if (!disabled) {
+      document.documentElement.classList.add("dark");
+      document.documentElement.classList.remove("light");
+      localStorage.setItem("theme", "dark");
+    }
+  }, [disabled]);
 
   return (
     <div className="translate-y-3 mr-6 max-md:translate-y-3 max-md::mr-3">
       <Switch
-        checked={enabled}
-        onChange={setEnabled}
-        className={`${enabled ? "bg-zinc-800" : "bg-zinc-200"}
-          relative inline-flex h-[28px] w-[56px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
-      >
-        <span className="sr-only">Use Dark Mode</span>
-        <span
-          aria-hidden="true"
-          className={`${enabled ? "translate-x-2 -translate-y-2.5" : "-translate-x-5 -translate-y-2.5"}
-            pointer-events-none inline-block h-[24px] w-[24px] transform rounded-full dark:bg-black bg-sky-600 shadow-lg ring-0 transition duration-200 ease-in-out`}
-        >
-          {enabled ? (
+        size="lg"
+        isSelected={disabled}
+        onValueChange={setDisabled}
+        thumbIcon={
+          disabled ? (
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
-              fill="white"
+              fill="black"
+              className="w-6 h-6"
+              role="button"
+            >
+              <path d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z" />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="black"
               className="w-6 h-6"
               role="button"
             >
@@ -55,19 +53,57 @@ export default function DarkMode() {
                 clipRule="evenodd"
               />
             </svg>
-          ) : (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="yellow"
-              className="w-6 h-6"
-              role="button"
-            >
-              <path d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z" />
-            </svg>
-          )}
-        </span>
-      </Switch>
+          )
+        }
+        startContent={
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="white"
+            className="w-6 h-6"
+            role="button"
+          >
+            <path
+              fillRule="evenodd"
+              d="M9.528 1.718a.75.75 0 01.162.819A8.97 8.97 0 009 6a9 9 0 009 9 8.97 8.97 0 003.463-.69.75.75 0 01.981.98 10.503 10.503 0 01-9.694 6.46c-5.799 0-10.5-4.701-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 01.818.162z"
+              clipRule="evenodd"
+            />
+          </svg>
+        }
+        endContent={
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="white"
+            className="w-6 h-6"
+            role="button"
+          >
+            <path d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z" />
+          </svg>
+        }
+      />
     </div>
   );
 }
+// <svg
+//   xmlns="http://www.w3.org/2000/svg"
+//   viewBox="0 0 24 24"
+//   fill="white"
+//   className="w-6 h-6"
+//   role="button"
+// >
+//   <path
+//     fillRule="evenodd"
+//     d="M9.528 1.718a.75.75 0 01.162.819A8.97 8.97 0 009 6a9 9 0 009 9 8.97 8.97 0 003.463-.69.75.75 0 01.981.98 10.503 10.503 0 01-9.694 6.46c-5.799 0-10.5-4.701-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 01.818.162z"
+//     clipRule="evenodd"
+//   />
+// </svg>;
+// <svg
+//   xmlns="http://www.w3.org/2000/svg"
+//   viewBox="0 0 24 24"
+//   fill="yellow"
+//   className="w-6 h-6"
+//   role="button"
+// >
+//   <path d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z" />
+// </svg>;
