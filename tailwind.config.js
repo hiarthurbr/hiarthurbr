255494bbfd740a5315fa483e2f60e9f54925f38d
Ar7hurz1nh0 <arthurbr@cdmd.dev>
2024-07-28 15:58:58 -0300

    moved to src directory

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,7 @@ import { nextui } from "@nextui-org/react";
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
