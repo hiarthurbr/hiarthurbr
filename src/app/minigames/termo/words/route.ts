5c863c5d12d6c5c2f24c53a0acd18133dbd89050
Ar7hurz1nh0 <arthurbr@cdmd.dev>
2024-07-30 15:20:54 -0300

    fixed caching, again?

--- src/app/minigames/termo/words/route.ts
+++ src/app/minigames/termo/words/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { kv } from "@vercel/kv";
 import { WORD_KEY } from "@lib/const";
-import { gen_words } from "@lib/gen_words";
+import type { gen_words } from "@lib/gen_words";
 
 export const runtime = "edge";
 
@@ -9,48 +9,21 @@ export async function GET(request: Request) {
   const now = new Date();
   const midnight = new Date(now).setUTCHours(23, 59, 30, 0);
 
-  let words = await kv.get<
+  const words = (await kv.get<
     ReturnType<typeof gen_words> & {
       by: "edge" | "cron";
-      at?: number;
-      date?: string;
+      at: number;
+      date: string;
     }
-  >(WORD_KEY);
+  >(WORD_KEY))!;
 
   const headers = {
-    "Cache-Control": `public, max-age=${Math.floor((midnight - now.getTime()) / 1000)}, must-revalidate, immutable`,
-    Expires: new Date(midnight).toUTCString(),
-    Date: now.toUTCString(),
+    "Cache-Control": `public, max-age=${Math.floor((midnight - words.at) / 1000)}, must-revalidate, immutable`,
+    Expires: new Date(
+      new Date(words.at).setUTCHours(23, 59, 30, 0),
+    ).toUTCString(),
+    Date: new Date(words.at).toUTCString(),
   };
-
-  if (
-    words == null ||
-    words.at == null ||
-    typeof words.at !== "number" ||
-    now.getTime() - words.at >= 1000 * 60 * 60 * 24 - 1000 * 60
-    //                          ^^^^^^^^^^^^^^^^^^^   ^^^^^^^^^
-    //                          |> 24 hours           |> 1 minute
-  ) {
-    words = {
-      ...gen_words(),
-      by: "edge",
-      at: now.getTime(),
-      date: now.toISOString(),
-    };
-    await kv.set(WORD_KEY, words);
-
-    return NextResponse.json(words, {
-      status: 201,
-      headers,
-    });
-  }
-
-  headers["Cache-Control"] =
-    `public, max-age=${Math.floor((midnight - words.at) / 1000)}, must-revalidate, immutable`;
-  headers.Expires = new Date(
-    new Date(words.at).setUTCHours(23, 59, 30, 0),
-  ).toUTCString();
-  headers.Date = new Date(words.at).toUTCString();
 
   return NextResponse.json(words, {
     status: 200,
